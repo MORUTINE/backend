ec556be95627f415c263fb756482d0bb3ba9303a
zakie <realzzaekkii@gmail.com>
2025-11-20 19:46:16 +0900

    feat: ApiResponse<T>에 IntoResponse 구현 추가

--- backend/todo-api/src/common/response/api_response.rs
+++ backend/todo-api/src/common/response/api_response.rs
@@ -1,3 +1,4 @@
+use axum::{Json, response::IntoResponse, http::StatusCode};
 use common::constant::status::*;
 use common::error::error_reason::ErrorReason;
 use serde::Serialize;
@@ -8,6 +9,15 @@ pub struct ApiResponse<T> {
     pub code: &'static str,
     pub message: Option<&'static str>,
     pub data: Option<T>,
+}
+
+impl<T: Serialize> IntoResponse for ApiResponse<T> {
+    fn into_response(self) -> axum::response::Response {
+        let status = StatusCode::from_u16(self.status).unwrap_or(StatusCode::OK);
+        let body = Json(self);
+
+        (status, body).into_response()
+    }
 }
 
 impl<T: Serialize> ApiResponse<T> {
