3e2cdd53bbfff9720fb1ce324b3cd733dc2c17e2
Jaeyeong Choi <zzaekkii@gmail.com>
2025-11-29 05:09:05 +0900

    기존 공통 응답 제거 후 RESTful 응답 구조로 변경 + 소나 클라우드 에러 해결 (#13)
    * refactor: 성공 시엔 status code와 body만 있으면 되므로, 공통 응답 구조 삭제
    
    * refactor: 에러 response 변환에서 body 제거
    
    * refactor: 기존 헬스 체크 응답 구조 변환
    
    * fix: cargo fmt 적용
    
    * chore: sonarcloud 재적용
    
    * chore: sonarcloud 프로젝트키 재설정
    
    * chore: sonarcloud 커버리지 리포트 포맷 변경
    
    * chore: sonarcloud 커저리지 관련 코드 변경
    
    * chore: sonarcloud 환경변수 등록, xml로 변경
    
    * chore: sonarcloud scan 오류 고치기 시도 1
    
    * chore: sonarcloud scan 오류 고치기 시도 2
    
    * chore: sonarcloud scan 오류 고치기 시도 3 (메모리 문제)
    
    * chore: sonarcloud scan 오류 고치기 시도 4 (grcov 에러)
    
    * chore: sonarcloud 커버리지 리포트 에러 (단순화)
    
    * chore: sonarcloud 커버리지 리포트 에러 (단순화)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 2
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 3
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 4
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 5
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 6
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 7
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 8
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 9
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 10
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 11
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 12
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 13
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 14 - xml 확인
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 15 - xml 포맷 변경
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 16 - xml 포맷 변경
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 17 - xml 포맷 변경
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 18 - xml 버전
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 19 - xml 버전
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 20 - xml 버전
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 21 - xml 버전
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 22 - xml파일 내 태그
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 23 - xml파일 내 태그
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 24 - xml파일 내 태그
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 25 - xml파일 내 태그
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 26 - xml파일 내 태그
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 27 - xml파일 내 태그(gemi)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 28 - xml파일 내 태그(claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 29 (claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 30 (claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 31 (claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 32 (claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 33 (claud)
    
    * chore: sonarcloud 커버리지 리포트 에러 해결 시도 34 (claud)

--- morutine-api/src/domain/system/handlers.rs
+++ morutine-api/src/domain/system/handlers.rs
@@ -1,6 +1,7 @@
-use crate::common::response::api_response::ApiResponse;
+use axum::Json;
 use axum::response::IntoResponse;
+use serde_json::json;
 
 pub async fn health_check() -> impl IntoResponse {
-    ApiResponse::success("OK")
+    Json(json!({"status": "OK"}))
 }
