edd637379350422e80d6e155008e7973761ec55c
zakie <realzzaekkii@gmail.com>
2025-12-22 15:55:17 +0900

    feat: DI 컨테이너 생성 - todo_repository 의존성 주입, state 내부 흐름 정리

--- morutine-api/src/bootstrap/state.rs
+++ morutine-api/src/bootstrap/state.rs
@@ -1,43 +1,46 @@
-use axum::Router;
-use sea_orm::{ConnectOptions, Database, DatabaseConnection};
-use std::time::Duration;
-
 use crate::bootstrap::config::AppConfig;
+use axum::Router;
+use domain::todo::repository::todo_repository::TodoRepository;
+use infra::database::postgres::todo::todo_repository_impl::TodoRepositoryImpl;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use std::sync::Arc;
+use std::time::Duration;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub db: DatabaseConnection,
-    pub config: AppConfig,
+    pub todo_repository: Arc<dyn TodoRepository>,
 }
 
-pub async fn init_state(config: &AppConfig) -> AppState {
-    let db_url = config.database.to_postgres_url();
+impl AppState {
+    pub async fn new(config: &AppConfig) -> Self {
+        let db = Self::init_db(config).await;
+        let todo_repository = Arc::new(TodoRepositoryImpl::new(db.clone()));
 
-    let mut opt = ConnectOptions::new(db_url.clone());
-    opt.max_connections(10) // FIXME: 세팅값은 나중에 최적화
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(5))
-        .acquire_timeout(Duration::from_secs(5))
-        .idle_timeout(Duration::from_secs(30))
-        .max_lifetime(Duration::from_secs(600))
-        .sqlx_logging(true);
+        Self { todo_repository }
+    }
 
-    let db = Database::connect(opt)
-        .await
-        .expect("DB 연결에 실패 했습니다.");
+    async fn init_db(config: &AppConfig) -> DatabaseConnection {
+        let db_url = config.database.to_postgres_url();
 
-    AppState {
-        db,
-        config: config.clone(),
+        let mut opt = ConnectOptions::new(db_url.clone());
+        opt.max_connections(10) // FIXME: 세팅값은 나중에 최적화
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(5))
+            .acquire_timeout(Duration::from_secs(5))
+            .idle_timeout(Duration::from_secs(30))
+            .max_lifetime(Duration::from_secs(600))
+            .sqlx_logging(true);
+
+        Database::connect(opt)
+            .await
+            .expect("DB 연결에 실패 했습니다.")
     }
 }
 
 pub async fn run_server(app: Router, port: u16) {
     let addr = format!("0.0.0.0:{port}");
-
-    tracing::info!("서버가 {addr} 에서 실행 중");
+    tracing::info!("{addr} 에서 서버 실행 중");
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-
     axum::serve(listener, app).await.unwrap();
 }
--- morutine-api/src/main.rs
+++ morutine-api/src/main.rs
@@ -3,17 +3,17 @@ mod common;
 mod domain;
 mod routes;
 
+use crate::bootstrap::AppState;
 use crate::routes::create_app_router;
-use bootstrap::{init_logger, init_state, load_config, run_server};
+use bootstrap::{init_logger, load_config, run_server};
 
 #[tokio::main]
 async fn main() {
     let config = load_config();
     init_logger(&config);
 
-    let state = init_state(&config).await;
+    let state = AppState::new(&config).await;
 
     let router = create_app_router(&config).with_state(state);
-
     run_server(router, config.server.port).await;
 }
